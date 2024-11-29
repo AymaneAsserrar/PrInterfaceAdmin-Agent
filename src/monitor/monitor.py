60a2e0628d2d5c2c7e9623ef8d96380735f641e2
asserrar.aymane <aymane.asserrar@etu.uae.ac.ma>
2024-11-29 17:01:59 +0100

    Fixing monitor errors

--- src/monitor/monitor.py
+++ src/monitor/monitor.py
@@ -9,22 +9,38 @@ class MonitorTask:
     interval: int
     cpu_percent: list[float]
     num_cores: int
+    ram_percent: float
+    total_ram: float
+    available_ram: float
+    used_ram: float
+    free_ram: float
 
     def __init__(self) -> None:
         """
         Initialize the MonitorTask class.
-
         Add initialization tasks here like checks
         The monitoring interval is 3 seconds.
         """
         self.interval = 3
         self.num_cores = psutil.cpu_count(logical=False)
         self.cpu_percent = [0] * self.num_cores
+        self.ram_percent = 0
+        self._update_ram_metrics()
+
+    def _update_ram_metrics(self):
+        """Update RAM-related metrics."""
+        ram = psutil.virtual_memory()
+        self.ram_percent = ram.percent
+        self.total_ram = ram.total / (1024 * 1024)  # Convert to MB
+        self.available_ram = ram.available / (1024 * 1024)
+        self.used_ram = ram.used / (1024 * 1024)
+        self.free_ram = ram.free / (1024 * 1024)
 
     def monitor(self):
         """Continuously monitor and store the result in an attribute."""
         while True:
             self.cpu_percent = psutil.cpu_percent(percpu=True)
+            self._update_ram_metrics()
             time.sleep(self.interval)
 
     def __str__(self) -> str:
