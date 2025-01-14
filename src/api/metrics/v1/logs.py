cd3d9811a01656635b3034c0e5778300f306e2ef
asserrar.aymane <aymane.asserrar@etu.uae.ac.ma>
2025-01-14 19:14:12 +0100

    Modify the log endp to pass the log path from the environment

--- src/api/metrics/v1/logs.py
+++ src/api/metrics/v1/logs.py
@@ -1,12 +1,16 @@
 """
 This module defines API routes for log metrics.
 """
+import os
 from fastapi import APIRouter, HTTPException, status
 from domain.schemas import LogMetricsSchema, ExceptionResponseSchema
 from domain.services import LogService
 
 log_router = APIRouter()
 
+# Get log paths from environment variables with defaults
+ACCESS_LOG_PATH = os.getenv('ACCESS_LOG_PATH', '/app/logs/access.log')
+ERROR_LOG_PATH = os.getenv('ERROR_LOG_PATH', '/app/logs/error.log')
 
 @log_router.get(
     "/metrics",
@@ -26,7 +30,7 @@ async def get_log_metrics() -> LogMetricsSchema:
     """
     try:
         service = LogService()
-        return await service.get_log_metrics()
+        return await service.get_log_metrics(access_log_path=ACCESS_LOG_PATH, error_log_path=ERROR_LOG_PATH)
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
